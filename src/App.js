8a3d2c08d448c73e819fb77f16da819f91b5a144
Kunal <47500407+kunalpatil239@users.noreply.github.com>
2020-12-20 21:16:00 +0530

    updated style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 const emojiDictionary = {
@@ -19,30 +19,35 @@ const emojiDictionary = {
 
 export default function App() {
   const [meaning, setMeaning] = useState("");
-  function emojiInputHandler(event){
+  function emojiInputHandler(event) {
     const userInput = event.target.value;
     var meaning = emojiDictionary[userInput];
     if (meaning === undefined) meaning = "Meaning not found";
     setMeaning(meaning);
-
   }
   function onEmojiClick(emoji) {
     setMeaning(emojiDictionary[emoji]);
   }
   return (
     <div className="App">
-      <h1>Inside Out</h1>
+      <h1 className="heading">Do you know this Emoji?</h1>
+      <hr />
+      <h2>
+        {" "}
+        Enter any Emoji you want to know the meaning of. Or select from the
+        below mentioned set.
+      </h2>
       <input onChange={emojiInputHandler}></input>
-      <div className="meaning">{meaning}</div>
+      <h2 className="meaning">{meaning}</h2>
       <h2>Emojis We know</h2>
-      {Object.keys(emojiDictionary).map((item)=>(
+      {Object.keys(emojiDictionary).map((item) => (
         <span
-        onClick={() => onEmojiClick(item)}
-        key={item}
-        className="emoji-small"
-      >
-        {item}
-      </span>
+          onClick={() => onEmojiClick(item)}
+          key={item}
+          className="emoji-small"
+        >
+          {item}
+        </span>
       ))}
     </div>
   );
